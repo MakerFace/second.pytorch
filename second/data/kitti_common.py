433bd17793ca0b478fdaaf730dbf4942ecafdb59
MakerFace <chenqi@imudges.com>
2021-11-19 13:30:28 +0800

    modify roadside to kitti

--- second/data/kitti_common.py
+++ second/data/kitti_common.py
@@ -224,6 +224,13 @@ def get_kitti_image_info(path,
                 idx, path, training, relative_path=False)
             with open(calib_path, 'r') as f:
                 lines = f.readlines()
+            lines=["P0: 7.070493000000e+02 0.000000000000e+00 6.040814000000e+02 0.000000000000e+00 0.000000000000e+00 7.070493000000e+02 1.805066000000e+02 0.000000000000e+00 0.000000000000e+00 0.000000000000e+00 1.000000000000e+00 0.000000000000e+00",
+                   "P1: 7.070493000000e+02 0.000000000000e+00 6.040814000000e+02 -3.797842000000e+02 0.000000000000e+00 7.070493000000e+02 1.805066000000e+02 0.000000000000e+00 0.000000000000e+00 0.000000000000e+00 1.000000000000e+00 0.000000000000e+00",
+                   "P2: 7.070493000000e+02 0.000000000000e+00 6.040814000000e+02 4.575831000000e+01 0.000000000000e+00 7.070493000000e+02 1.805066000000e+02 -3.454157000000e-01 0.000000000000e+00 0.000000000000e+00 1.000000000000e+00 4.981016000000e-03",
+                   "P3: 7.070493000000e+02 0.000000000000e+00 6.040814000000e+02 -3.341081000000e+02 0.000000000000e+00 7.070493000000e+02 1.805066000000e+02 2.330660000000e+00 0.000000000000e+00 0.000000000000e+00 1.000000000000e+00 3.201153000000e-03",
+                   "R0_rect: 9.999128000000e-01 1.009263000000e-02 -8.511932000000e-03 -1.012729000000e-02 9.999406000000e-01 -4.037671000000e-03 8.470675000000e-03 4.123522000000e-03 9.999556000000e-01",
+                   "Tr_velo_to_cam: 6.927964000000e-03 -9.999722000000e-01 -2.757829000000e-03 -2.457729000000e-02 -1.162982000000e-03 2.749836000000e-03 -9.999955000000e-01 -6.127237000000e-02 9.999753000000e-01 6.931141000000e-03 -1.143899000000e-03 -3.321029000000e-01",
+                   "Tr_imu_to_velo: 9.999976000000e-01 7.553071000000e-04 -2.035826000000e-03 -8.086759000000e-01 -7.854027000000e-04 9.998898000000e-01 -1.482298000000e-02 3.195559000000e-01 2.024406000000e-03 1.482454000000e-02 9.998881000000e-01 -7.997231000000e-01"]
             P0 = np.array(
                 [float(info) for info in lines[0].split(' ')[1:13]]).reshape(
                     [3, 4])
@@ -642,24 +649,24 @@ def get_label_anno(label_path):
     #     content = []
     # else:
     content = [line.strip().split(' ') for line in lines]
-    num_objects = len([x[0] for x in content if x[0] != 'DontCare'])
-    annotations['name'] = np.array([x[0] for x in content])
+    num_objects = len([x[1] for x in content if x[1] != 'DontCare'])
+    annotations['name'] = np.array([x[1] for x in content])
     num_gt = len(annotations['name'])
-    annotations['truncated'] = np.array([float(x[1]) for x in content])
-    annotations['occluded'] = np.array([int(x[2]) for x in content])
-    annotations['alpha'] = np.array([float(x[3]) for x in content])
+    annotations['truncated'] = np.array([float(x[2]) for x in content])
+    annotations['occluded'] = np.array([int(x[3]) for x in content])
+    annotations['alpha'] = np.array([float(x[-1]) for x in content])
     annotations['bbox'] = np.array(
-        [[float(info) for info in x[4:8]] for x in content]).reshape(-1, 4)
+        [[float(0) for info in x[4:8]] for x in content]).reshape(-1, 4)
     # dimensions will convert hwl format to standard lhw(camera) format.
     annotations['dimensions'] = np.array(
-        [[float(info) for info in x[8:11]] for x in content]).reshape(
+        [[float(info) for info in x[4:7]] for x in content]).reshape(
             -1, 3)[:, [2, 0, 1]]
     annotations['location'] = np.array(
-        [[float(info) for info in x[11:14]] for x in content]).reshape(-1, 3)
+        [[float(info) for info in x[7:10]] for x in content]).reshape(-1, 3)
     annotations['rotation_y'] = np.array(
-        [float(x[14]) for x in content]).reshape(-1)
+        [float(0) for x in content]).reshape(-1)
     if len(content) != 0 and len(content[0]) == 16:  # have score
-        annotations['score'] = np.array([float(x[15]) for x in content])
+        annotations['score'] = np.array([float(0) for x in content])
     else:
         annotations['score'] = np.zeros((annotations['bbox'].shape[0], ))
     index = list(range(num_objects)) + [-1] * (num_gt - num_objects)
